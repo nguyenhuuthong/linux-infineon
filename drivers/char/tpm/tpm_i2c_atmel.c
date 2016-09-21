2ee2754da63a168e3fb25f9d88b1f484550f549d
Thong Nguyen <thong2k9@gmail.com>
2016-09-21 14:34:52 +0700

    support tpm i2c atmel

--- drivers/char/tpm/tpm_i2c_atmel.c
+++ drivers/char/tpm/tpm_i2c_atmel.c
@@ -2,229 +2,338 @@
  * ATMEL I2C TPM AT97SC3204T
  *
  * Copyright (C) 2012 V Lab Technologies
- *  Teddy Reed <teddy@prosauce.org>
- * Copyright (C) 2013, Obsidian Research Corp.
- *  Jason Gunthorpe <jgunthorpe@obsidianresearch.com>
- * Device driver for ATMEL I2C TPMs.
  *
- * Teddy Reed determined the basic I2C command flow, unlike other I2C TPM
- * devices the raw TCG formatted TPM command data is written via I2C and then
- * raw TCG formatted TPM command data is returned via I2C.
+ * Authors:
+ * Teddy Reed <teddy@prosauce.org>
  *
- * TGC status/locality/etc functions seen in the LPC implementation do not
- * seem to be present.
+ * Device driver for TCG/TCPA TPM (trusted platform module).
+ * Specifications at www.trustedcomputinggroup.org
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
+ * This device driver implements the TPM interface as defined in
+ * the TCG TPM Interface Spec version 1.2.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * It is based on the Linux Infineon TIS 12C TPM driver from Peter Huewe,
+ * the original tpm_tis device driver from Leendert van Dorn and Kyleen Hall,
+ * and code provided from Atmel's Application Group, Crypto Products Division
+ * and Max R. May.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see http://www.gnu.org/licenses/>.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, version 2 of the
+ * License.
  */
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
-#include <linux/slab.h>
 #include <linux/i2c.h>
+#include <asm-generic/errno.h>
+
 #include "tpm.h"
 
-#define I2C_DRIVER_NAME "tpm_i2c_atmel"
+/* Specific to Atmel I2C TPM */
+#define TPM_BUFSIZE 1024
 
-#define TPM_I2C_SHORT_TIMEOUT  750     /* ms */
-#define TPM_I2C_LONG_TIMEOUT   2000    /* 2 sec */
+/* Not included in older tpm.h */
+#ifndef TPM_HEADER_SIZE
+#define TPM_HEADER_SIZE 10
+#endif
 
-#define ATMEL_STS_OK 1
-
-struct priv_data {
-	size_t len;
-	/* This is the amount we read on the first try. 25 was chosen to fit a
-	 * fair number of read responses in the buffer so a 2nd retry can be
-	 * avoided in small message cases. */
-	u8 buffer[sizeof(struct tpm_output_header) + 25];
+struct tpm_i2c_atmel_dev {
+	struct i2c_client *client;
+	u8 buf[TPM_BUFSIZE];
+	struct tpm_chip *chip;
 };
 
-static int i2c_atmel_send(struct tpm_chip *chip, u8 *buf, size_t len)
+struct tpm_i2c_atmel_dev tpm_dev;
+static struct i2c_driver tpm_tis_i2c_driver;
+
+static u8 	tpm_i2c_read(u8 *buffer, u16 len);
+
+static void tpm_tis_i2c_ready (struct tpm_chip *chip);
+static u8 	tpm_tis_i2c_status (struct tpm_chip *chip);
+static int 	tpm_tis_i2c_recv (struct tpm_chip *chip, u8 *buf, size_t count);
+static int 	tpm_tis_i2c_send (struct tpm_chip *chip, u8 *buf, size_t count);
+
+static int tpm_tis_i2c_probe (struct i2c_client *client, const struct i2c_device_id *id);
+static int tpm_tis_i2c_remove (struct i2c_client *client);
+static int tpm_tis_i2c_init (struct device *dev);
+
+enum tis_defaults {
+	TIS_SHORT_TIMEOUT = 750,	/* ms */
+	TIS_LONG_TIMEOUT = 2000,	/* 2 sec */
+};
+
+static u8 tpm_i2c_read(u8 *buffer, u16 len)
 {
-	struct priv_data *priv = chip->vendor.priv;
-	struct i2c_client *client = to_i2c_client(chip->pdev);
-	s32 status;
-
-	priv->len = 0;
-
-	if (len <= 2)
-		return -EIO;
-
-	status = i2c_master_send(client, buf, len);
-
-	dev_dbg(chip->pdev,
-		"%s(buf=%*ph len=%0zx) -> sts=%d\n", __func__,
-		(int)min_t(size_t, 64, len), buf, len, status);
-	return status;
-}
-
-static int i2c_atmel_recv(struct tpm_chip *chip, u8 *buf, size_t count)
-{
-	struct priv_data *priv = chip->vendor.priv;
-	struct i2c_client *client = to_i2c_client(chip->pdev);
-	struct tpm_output_header *hdr =
-		(struct tpm_output_header *)priv->buffer;
-	u32 expected_len;
 	int rc;
+	u32 trapdoor = 0;
+	const u32 trapdoor_limit = 60000; /* 5min with base 5mil seconds */
 
-	if (priv->len == 0)
-		return -EIO;
+	/** Read into buffer, of size len **/
+	struct i2c_msg msg1 = {
+		.addr = tpm_dev.client->addr,
+		.flags = I2C_M_RD,
+		.len = len,
+		.buf = buffer
+	};
 
-	/* Get the message size from the message header, if we didn't get the
-	 * whole message in read_status then we need to re-read the
-	 * message. */
-	expected_len = be32_to_cpu(hdr->length);
-	if (expected_len > count)
-		return -ENOMEM;
+	/** should lock the device **/
+	/** locking is performed by the i2c_transfer function **/
 
-	if (priv->len >= expected_len) {
-		dev_dbg(chip->pdev,
-			"%s early(buf=%*ph count=%0zx) -> ret=%d\n", __func__,
-			(int)min_t(size_t, 64, expected_len), buf, count,
-			expected_len);
-		memcpy(buf, priv->buffer, expected_len);
-		return expected_len;
-	}
+	if (!tpm_dev.client->adapter->algo->master_xfer)
+		return -EOPNOTSUPP;
 
-	rc = i2c_master_recv(client, buf, expected_len);
-	dev_dbg(chip->pdev,
-		"%s reread(buf=%*ph count=%0zx) -> ret=%d\n", __func__,
-		(int)min_t(size_t, 64, expected_len), buf, count,
-		expected_len);
+	do {
+		rc = i2c_transfer(tpm_dev.client->adapter, &msg1, 1);
+		if (rc > 0x00) /* successful read */
+			break;
+		trapdoor++;
+		msleep(5);
+	} while (trapdoor < trapdoor_limit);
+
+	/** failed to read **/
+	if (trapdoor >= trapdoor_limit)
+		return -EFAULT;
+
 	return rc;
 }
 
-static void i2c_atmel_cancel(struct tpm_chip *chip)
+static int tpm_tis_i2c_recv (struct tpm_chip *chip, u8 *buf, size_t count)
 {
-	dev_err(chip->pdev, "TPM operation cancellation was requested, but is not supported");
+	int rc = 0;
+	u16 expected;
+
+	memset(tpm_dev.buf, 0x00, TPM_BUFSIZE);
+	rc = tpm_i2c_read(tpm_dev.buf, TPM_HEADER_SIZE); /* returns status of read */
+
+	expected = tpm_dev.buf[4];
+	expected = expected << 8;
+	expected += tpm_dev.buf[5]; /* should never be > TPM_BUFSIZE */
+
+	if (expected <= TPM_HEADER_SIZE) {
+		/* finished here */
+		goto to_user;
+	}
+
+	/* Read again, the expected number of bytes */
+	rc = tpm_i2c_read(tpm_dev.buf, expected);
+
+to_user:
+	memcpy(buf, tpm_dev.buf, expected);
+
+	return expected;
 }
 
-static u8 i2c_atmel_read_status(struct tpm_chip *chip)
+static int tpm_tis_i2c_send (struct tpm_chip *chip, u8 *buf, size_t count)
 {
-	struct priv_data *priv = chip->vendor.priv;
-	struct i2c_client *client = to_i2c_client(chip->pdev);
 	int rc;
 
-	/* The TPM fails the I2C read until it is ready, so we do the entire
-	 * transfer here and buffer it locally. This way the common code can
-	 * properly handle the timeouts. */
-	priv->len = 0;
-	memset(priv->buffer, 0, sizeof(priv->buffer));
+	/** Write to tpm_dev.buf, size count **/
+	struct i2c_msg msg1 = {
+		.addr = tpm_dev.client->addr,
+		.flags = 0,
+		.len = count,
+		.buf = tpm_dev.buf
+	};
 
+	rc = -EIO;
+	if (count > TPM_BUFSIZE) {
+		return -EINVAL;
+	}
 
-	/* Once the TPM has completed the command the command remains readable
-	 * until another command is issued. */
-	rc = i2c_master_recv(client, priv->buffer, sizeof(priv->buffer));
-	dev_dbg(chip->pdev,
-		"%s: sts=%d", __func__, rc);
+	/** should lock the device **/
+	/** locking is performed by the i2c_transfer function **/
+
+	memset(tpm_dev.buf, 0x00, TPM_BUFSIZE);
+	/* should add sanitization */
+	memcpy(tpm_dev.buf, buf, count);
+
+	rc = i2c_transfer(tpm_dev.client->adapter, &msg1, 1);
+
 	if (rc <= 0)
-		return 0;
+		return -EIO;
 
-	priv->len = rc;
-
-	return ATMEL_STS_OK;
+	return count;
 }
 
-static bool i2c_atmel_req_canceled(struct tpm_chip *chip, u8 status)
+static u8 tpm_tis_i2c_status (struct tpm_chip *chip)
 {
-	return false;
+	return 1; /* not a timeout */
 }
 
-static const struct tpm_class_ops i2c_atmel = {
-	.status = i2c_atmel_read_status,
-	.recv = i2c_atmel_recv,
-	.send = i2c_atmel_send,
-	.cancel = i2c_atmel_cancel,
-	.req_complete_mask = ATMEL_STS_OK,
-	.req_complete_val = ATMEL_STS_OK,
-	.req_canceled = i2c_atmel_req_canceled,
+static void tpm_tis_i2c_ready (struct tpm_chip *chip)
+{
+	/* nothing */
+}
+
+/* from Infineon driver */
+static const struct file_operations tis_ops = {
+	.owner = THIS_MODULE,
+	.llseek = no_llseek,
+	.open = tpm_open,
+	.read = tpm_read,
+	.write = tpm_write,
+	.release = tpm_release,
 };
 
-static int i2c_atmel_probe(struct i2c_client *client,
-			   const struct i2c_device_id *id)
+static DEVICE_ATTR(pubek, S_IRUGO, tpm_show_pubek, NULL);
+static DEVICE_ATTR(pcrs, S_IRUGO, tpm_show_pcrs, NULL);
+static DEVICE_ATTR(enabled, S_IRUGO, tpm_show_enabled, NULL);
+static DEVICE_ATTR(active, S_IRUGO, tpm_show_active, NULL);
+static DEVICE_ATTR(owned, S_IRUGO, tpm_show_owned, NULL);
+static DEVICE_ATTR(temp_deactivated, S_IRUGO, tpm_show_temp_deactivated, NULL);
+static DEVICE_ATTR(caps, S_IRUGO, tpm_show_caps_1_2, NULL);
+static DEVICE_ATTR(cancel, S_IWUSR | S_IWGRP, NULL, tpm_store_cancel);
+static DEVICE_ATTR(durations, S_IRUGO, tpm_show_durations, NULL);
+static DEVICE_ATTR(timeouts, S_IRUGO, tpm_show_timeouts, NULL);
+
+static struct attribute *tis_attrs[] = {
+	&dev_attr_pubek.attr,
+	&dev_attr_pcrs.attr,
+	&dev_attr_enabled.attr,
+	&dev_attr_active.attr,
+	&dev_attr_owned.attr,
+	&dev_attr_temp_deactivated.attr,
+	&dev_attr_caps.attr,
+	&dev_attr_cancel.attr,
+	&dev_attr_durations.attr,
+	&dev_attr_timeouts.attr,
+	NULL,
+};
+
+static struct attribute_group tis_attr_grp = {
+	.attrs = tis_attrs
+};
+
+static struct tpm_vendor_specific tpm_tis_i2c = {
+	.status = tpm_tis_i2c_status,
+	.recv = tpm_tis_i2c_recv,
+	.send = tpm_tis_i2c_send,
+	.cancel = tpm_tis_i2c_ready,
+	/*.req_complete_mask = TPM_STS_DATA_AVAIL | TPM_STS_VALID,
+	.req_complete_val = TPM_STS_DATA_AVAIL | TPM_STS_VALID,
+	.req_canceled = TPM_STS_COMMAND_READY,*/
+	.attr_group = &tis_attr_grp,
+	.miscdev.fops = &tis_ops,
+};
+/* end from Infineon */
+
+static struct i2c_device_id tpm_tis_i2c_table[] = {
+		{ "tpm_i2c_atmel", 0 },
+		{ }
+};
+
+MODULE_DEVICE_TABLE(i2c, tpm_tis_i2c_table);
+
+static int tpm_tis_i2c_probe (struct i2c_client *client,
+		const struct i2c_device_id *id)
 {
-	struct tpm_chip *chip;
-	struct device *dev = &client->dev;
+	int rc;
 
-	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C))
+	if (tpm_dev.client != NULL)
+		return -EBUSY; /* only 1 TPM per-system */
+
+	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C)) {
+		dev_err(&client->dev, "no algorithms associated to i2c bus\n");
 		return -ENODEV;
+	}
 
-	chip = tpmm_chip_alloc(dev, &i2c_atmel);
-	if (IS_ERR(chip))
-		return PTR_ERR(chip);
-
-	chip->vendor.priv = devm_kzalloc(dev, sizeof(struct priv_data),
-					 GFP_KERNEL);
-	if (!chip->vendor.priv)
-		return -ENOMEM;
-
-	/* Default timeouts */
-	chip->vendor.timeout_a = msecs_to_jiffies(TPM_I2C_SHORT_TIMEOUT);
-	chip->vendor.timeout_b = msecs_to_jiffies(TPM_I2C_LONG_TIMEOUT);
-	chip->vendor.timeout_c = msecs_to_jiffies(TPM_I2C_SHORT_TIMEOUT);
-	chip->vendor.timeout_d = msecs_to_jiffies(TPM_I2C_SHORT_TIMEOUT);
-	chip->vendor.irq = 0;
-
-	/* There is no known way to probe for this device, and all version
-	 * information seems to be read via TPM commands. Thus we rely on the
-	 * TPM startup process in the common code to detect the device. */
-	if (tpm_get_timeouts(chip))
+	/* not a good implementation, will match any i2c device that responds. */
+	rc = i2c_smbus_read_byte(client);
+	if (rc < 0x00) {
 		return -ENODEV;
+	}
 
-	if (tpm_do_selftest(chip))
-		return -ENODEV;
+	client->driver = &tpm_tis_i2c_driver;
+	tpm_dev.client = client;
+	rc = tpm_tis_i2c_init(&client->dev);
+	if (rc != 0) {
+		client->driver = NULL;
+		tpm_dev.client = NULL;
+		rc = -ENODEV;
+	}
 
-	return tpm_chip_register(chip);
+	return rc;
 }
 
-static int i2c_atmel_remove(struct i2c_client *client)
+static int tpm_tis_i2c_remove(struct i2c_client *client)
 {
-	struct device *dev = &(client->dev);
-	struct tpm_chip *chip = dev_get_drvdata(dev);
-	tpm_chip_unregister(chip);
+	struct tpm_chip *chip = tpm_dev.chip;
+
+	/* close file handles */
+	tpm_dev_vendor_release(chip);
+	/* remove hardware */
+	tpm_remove_hardware(chip->dev);
+
+	/* reset pointers */
+	chip->dev->release = NULL;
+	chip->release = NULL;
+	tpm_dev.client = NULL;
+	dev_set_drvdata(chip->dev, chip);
+
 	return 0;
 }
 
-static const struct i2c_device_id i2c_atmel_id[] = {
-	{I2C_DRIVER_NAME, 0},
-	{}
-};
-MODULE_DEVICE_TABLE(i2c, i2c_atmel_id);
+static int tpm_tis_i2c_init (struct device *dev)
+{
+	int rc = 0;
+	struct tpm_chip *chip;
 
-#ifdef CONFIG_OF
-static const struct of_device_id i2c_atmel_of_match[] = {
-	{.compatible = "atmel,at97sc3204t"},
-	{},
-};
-MODULE_DEVICE_TABLE(of, i2c_atmel_of_match);
-#endif
+	chip = tpm_register_hardware(dev, &tpm_tis_i2c);
+	if (!chip) {
+		return -ENODEV;
+	}
 
-static SIMPLE_DEV_PM_OPS(i2c_atmel_pm_ops, tpm_pm_suspend, tpm_pm_resume);
+	/* Disable interrupts */
+	chip->vendor.irq = 0;
 
-static struct i2c_driver i2c_atmel_driver = {
-	.id_table = i2c_atmel_id,
-	.probe = i2c_atmel_probe,
-	.remove = i2c_atmel_remove,
+	/* Set default timeouts */
+	chip->vendor.timeout_a = msecs_to_jiffies(TIS_SHORT_TIMEOUT);
+	chip->vendor.timeout_b = msecs_to_jiffies(TIS_LONG_TIMEOUT);
+	chip->vendor.timeout_c = msecs_to_jiffies(TIS_SHORT_TIMEOUT);
+	chip->vendor.timeout_d = msecs_to_jiffies(TIS_SHORT_TIMEOUT);
+
+	/* The device responds to an unsolicited read with 0x1 0x2 0x3 ... */
+
+	dev_info(dev, "1.2 TPM");
+
+	tpm_dev.chip = chip;
+	memset(tpm_dev.buf, 0x00, TPM_BUFSIZE);
+
+	tpm_get_timeouts(chip);
+	/* tpm_do_selftest(chip); */
+
+	return rc;
+}
+
+static struct i2c_driver tpm_tis_i2c_driver = {
 	.driver = {
-		.name = I2C_DRIVER_NAME,
+		.name = "tpm_i2c_atmel",
 		.owner = THIS_MODULE,
-		.pm = &i2c_atmel_pm_ops,
-		.of_match_table = of_match_ptr(i2c_atmel_of_match),
 	},
+	.probe = tpm_tis_i2c_probe,
+	.remove = tpm_tis_i2c_remove, /* __devexit_p() */
+	.id_table = tpm_tis_i2c_table,
 };
 
-module_i2c_driver(i2c_atmel_driver);
+static int __init tpm_tis_i2c_modinit(void)
+{
+	int rc = 0;
 
-MODULE_AUTHOR("Jason Gunthorpe <jgunthorpe@obsidianresearch.com>");
-MODULE_DESCRIPTION("Atmel TPM I2C Driver");
+	rc = i2c_add_driver(&tpm_tis_i2c_driver);
+	return rc;
+}
+
+static void __exit tpm_tis_i2c_exit(void)
+{
+	i2c_del_driver(&tpm_tis_i2c_driver);
+}
+
+module_init(tpm_tis_i2c_modinit);
+module_exit(tpm_tis_i2c_exit);
+
+MODULE_AUTHOR("Teddy Reed <teddy@prosauce.org>");
+MODULE_DESCRIPTION("Atmel AT97SC3204T I2C TPM");
+MODULE_VERSION("1.1");
 MODULE_LICENSE("GPL");
